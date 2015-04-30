44d6deb578bdf4210726555da8f767e2070f0ad1
Thu Thôi <camthu.vuive@gmail.com>
2015-04-30 14:53:17 +0700

    Them project

---
+++ ASP.NET_MVC/Controllers/CalcController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC.Controllers
+{
+    public class CalcController : Controller
+    {
+        //
+        // GET: /Calc/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int x, int y, string @operator)
+        {
+            switch (@operator)
+            {
+                case "+":
+                    {
+                        int s = x + y;
+                        ViewBag.RET = s;
+                        break;
+                    }
+                case "-":
+                    {
+                        int s = x - y;
+                        ViewBag.RET = s;
+                        break;
+                    }
+                case "*":
+                    {
+                        int s = x * y;
+                        ViewBag.RET = s;
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ASP.NET_MVC/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/GetID/7 or /Home/GetID?id=7
+
+        public ActionResult GetID(int? id)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Ret = id;
+            return View();
+        }
+
+        //
+        // GET: /Home/GetIDName?id=7&name=thu
+
+        public ActionResult GetIDName(int? id, string name)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ID = id;
+            ViewBag.Name = name;
+            return View();
+        }
+
+
+    }
+}
